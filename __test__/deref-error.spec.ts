e7ce3613d927bf5549d95a4bf7f14eca650390e1
Tong Li <tongli0809@gmail.com>
2020-03-14 19:58:22 +1100

    remove redudant import

--- __test__/deref-error.spec.ts
+++ __test__/deref-error.spec.ts
@@ -1,6 +1,4 @@
 import ChowChow from '../src';
-import ChowError, { ResponseValidationError } from '../src/error';
-import {ResponseMeta} from '../src/compiler';
 
 const fixture = require('./fixtures/deref-error.json');
 
